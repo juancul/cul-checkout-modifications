9a0fdf9dfe9bb772a59cdd999396fb9588b19051
muycul <48526137+muycul@users.noreply.github.com>
2021-07-21 22:42:35 -0500

    add inspectlet
    Add the inspectlet code to not logged in checkout

--- cul-checkout-modifications.php
+++ cul-checkout-modifications.php
@@ -159,6 +159,17 @@ function custom_override_checkout_fields($fields) {
     }*/
 
     else {
+        echo '<!-- Begin Inspectlet Asynchronous Code -->
+            <script type="text/javascript">
+            (function() {
+            window.__insp = window.__insp || [];
+            __insp.push(["wid", 1558197494]);
+            var ldinsp = function(){
+            if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement("script"); insp.type = "text/javascript"; insp.async = true; insp.id = "inspsync"; insp.src = ("https:" == document.location.protocol ? "https" : "http") + "://cdn.inspectlet.com/inspectlet.js?wid=1558197494&r=" + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(insp, x); };
+            setTimeout(ldinsp, 0);
+            })();
+            </script>
+            <!-- End Inspectlet Asynchronous Code -->';
         //Close all rentals
         /*echo ' <style>
                     .mwb_upsell_offer_parent_wrapper { 
